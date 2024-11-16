0bdc9de513928b3e15eeb16168c3a699d8f5c5da
Shivam Gupta <87187728+ShivamGupta-08@users.noreply.github.com>
2024-11-16 09:37:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.post('/update-data', async (req, res) => {
 
 // Periodically check if any helmet is off
 setInterval(async () => {
-    const threshold = 4000; // 10 seconds timeout for helmet off
+    const threshold = 8000; // 10 seconds timeout for helmet off
     const now = Date.now();
 
     try {
@@ -89,7 +89,7 @@ setInterval(async () => {
     } catch (error) {
         console.error('Error checking helmet statuses:', error);
     }
-}, 2000);
+}, 4000);
 
 // // Endpoint to retrieve helmet data by helmetId
 // GET endpoint to retrieve helmet data by helmetId
